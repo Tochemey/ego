2df52573480ca6ac716e65b9ccdc7a5784404a86
Mohamad Fadhil <piukul@yahoo.com.my>
2024-12-17 15:48:55 +0800

    update sql migration script

--- resources/durablestore_postgres.sql
+++ resources/durablestore_postgres.sql
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS states_store
     shard_number    BIGINT                NOT NULL,
 
     PRIMARY KEY (persistence_id, version_number)
-   CREATE INDEX IF NOT EXISTS idx_states_store_persistence_id ON events_store(persistence_id);
-   CREATE INDEX IF NOT EXISTS idx_states_store_version_number ON events_store(version_number);
 );
+
+CREATE INDEX IF NOT EXISTS idx_states_store_persistence_id ON states_store(persistence_id);
+CREATE INDEX IF NOT EXISTS idx_states_store_version_number ON states_store(version_number);
